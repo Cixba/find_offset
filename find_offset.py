7c17679c7b6a2182f39039ece28177589b21d1d1
Baciu Alex <36000906+Cixba@users.noreply.github.com>
2022-05-10 11:02:28 +0300

    Update find_offset.py

--- find_offset.py
+++ find_offset.py
@@ -1,61 +1,22 @@
-from pwn import *
-import sys
+import gdb
+import os
+	
 
-context.log_level = 'ERROR'
-		
-def try_input2(left, right):
-	found = 0
-	interval = []
-		
-	while found == 0:
-		p = process("./" + sys.argv[1])
-		
-		try:
-			p.sendline(b"A" * right)
-			resp = p.recv()
-			
-			left = right
-			right += 1000
-		except:
-			p.close()
-			found = 1
-			interval = [left, right]
+def find_offset():			
+	pattern='''aaaaaaaabaaaaaaacaaaaaaadaaaaaaaeaaaaaaafaaaaaaagaaaaaaahaaaaaaaiaaaaaaajaaaaaaakaaaaaaalaaaaaaamaaaaaaanaaaaaaaoaaaaaaapaaaaaaaqaaaaaaaraaaaaaasaaaaaaataaaaaaauaaaaaaavaaaaaaawaaaaaaaxaaaaaaayaaaaaaazaaaaaabbaaaaaabcaaaaaabdaaaaaabeaaaaaabfaaaaaabgaaaaaabhaaaaaabiaaaaaabjaaaaaabkaaaaaablaaaaaabmaaaaaabnaaaaaaboaaaaaabpaaaaaabqaaaaaabraaaaaabsaaaaaabtaaaaaabuaaaaaabvaaaaaabwaaaaaabxaaaaaabyaaaaaabzaaaaaacbaaaaaaccaaaaaacdaaaaaaceaaaaaacfaaaaaacgaaaaaachaaaaaaciaaaaaacjaaaaaackaaaaaaclaaaaaacmaaaaaacnaaaaaacoaaaaaacpaaaaaacqaaaaaacraaaaaacsaaaaaactaaaaaacuaaaaaacvaaaaaacwaaaaaacxaaaaaacyaaaaaaczaaaaaadbaaaaaadcaaaaaaddaaaaaadeaaaaaadfaaaaaadgaaaaaadhaaaaaadiaaaaaadjaaaaaadkaaaaaadlaaaaaadmaaaaaadnaaaaaadoaaaaaadpaaaaaadqaaaaaadraaaaaadsaaaaaadtaaaaaaduaaaaaadvaaaaaadwaaaaaadxaaaaaadyaaaaaadzaaaaaaebaaaaaaecaaaaaaedaaaaaaeeaaaaaaefaaaaaaegaaaaaaehaaaaaaeiaaaaaaejaaaaaaekaaaaaaelaaaaaaemaaaaaaenaaaaaaeoaaaaaaepaaaaaaeqaaaaaaeraaaaaaesaaaaaaetaaaaaaeuaaaaaaevaaaaaaewaaaaaaexaaaaaaeyaaaaaae'''
 	
-	print(f"Found interval: {interval}")
-	
-	found = 0
-	
-	while True:
-		if right - left <= 10:
-			break
-			
-		p = process("./" + sys.argv[1])
-			
-		try:
-			center = int((right + left) / 2)
-			p.sendline(b"A" * center)
-			resp = p.recv()
-			
-			left = center
-		except:
-			p.close()
-			right = center
-			
-	for i in range(left, right + 1):
-		p = process("./" + sys.argv[1])
+	with open('input.txt', 'w') as f:
+		f.write(pattern)
 		
-		try:
-			p.sendline(b"A" * i)
-			resp = p.recv()
-		except:
-			p.close()
-			print(f"Found offset at {i + 1}")
-			break
+	gdb.execute('run < input.txt')
+	overflow = gdb.execute('x/s $rsp', to_string=True).split(':')
+	
+	overflow = overflow[1][2:]
+	
+	pattern_to_search = overflow[:8]
+	gdb.execute(f'pattern search {pattern_to_search}')
+	
 
 if __name__ == "__main__":
-	if len(sys.argv) == 2:
-		try_input2(1, 1000)
-			
-	else:
-		print("Usage: python3 find_offset.py name_of_your_application")
-	
+	find_offset()
+		
